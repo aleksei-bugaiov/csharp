19d4389201fbd93dd04b6e12c006eb3cb7cad657
oleksii.buhaiov <oleksii.buhaiov@pm.bet>
2021-12-21 14:50:32 +0200

    added thread starvation sample

---
+++ src/CSharp.ThreadStarvation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.ThreadStarvation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(Environment.ProcessorCount);
+
+            ThreadPool.SetMinThreads(8, 8);
+
+            Task.Factory.StartNew(Producer, TaskCreationOptions.None);
+
+            Console.ReadLine();
+        }
+
+        private static void Producer()
+        {
+            while (true)
+            {
+                Process();
+
+                Thread.Sleep(50);
+            }
+        }
+
+        private static async Task Process()
+        {
+            await Task.Yield();
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                tcs.SetResult(true);
+            });
+
+            tcs.Task.Wait();
+
+            Console.WriteLine($"Ended: {DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
+
